8487876c0eabf7dcee80690db814ab8579171102
sathyadev <tosathyadev@gmail.com>
2021-03-26 21:30:27 +0530

    Added vpc & ec2 codes

---
+++ ec2s-vpc/instance.tf
@@ -0,0 +1,52 @@
+resource "aws_instance" "example" {
+  ami           = var.AMIS[var.AWS_REGION]
+  instance_type = "t2.micro"
+
+  # the VPC subnet
+  subnet_id = aws_subnet.main-public-1.id
+
+  # the security group
+  vpc_security_group_ids = [aws_security_group.allow-ssh.id]
+
+  # the public SSH key
+  key_name = aws_key_pair.mykeypair.key_name
+}
+resource "aws_instance" "example1" {
+  ami           = var.AMIS[var.AWS_REGION]
+  instance_type = "t2.micro"
+
+  # the VPC subnet
+  subnet_id = aws_subnet.main-public-2.id
+
+  # the security group
+  vpc_security_group_ids = [aws_security_group.allow-ssh.id]
+
+  # the public SSH key
+  key_name = aws_key_pair.mykeypair.key_name
+}
+resource "aws_instance" "example2" {
+  ami           = var.AMIS[var.AWS_REGION]
+  instance_type = "t2.micro"
+
+  # the VPC subnet
+  subnet_id = aws_subnet.main-private-1.id
+
+  # the security group
+  vpc_security_group_ids = [aws_security_group.allow-ssh.id]
+
+  # the public SSH key
+  key_name = aws_key_pair.mykeypair.key_name
+}
+resource "aws_instance" "example3" {
+  ami           = var.AMIS[var.AWS_REGION]
+  instance_type = "t2.micro"
+
+  # the VPC subnet
+  subnet_id = aws_subnet.main-private-2.id
+
+  # the security group
+  vpc_security_group_ids = [aws_security_group.allow-ssh.id]
+
+  # the public SSH key
+  key_name = aws_key_pair.mykeypair.key_name
+}
---
+++ vpc-3-subnets/provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+access_key = "XXXXXXXXXXXXXXXXXx"
+secret_key = "XXXXXXXXXXXXXXXXXXXXXXX"
+region = "ap-south-1" 
+}
